61252f0d963cd7e2773caad3e24cfbba538e5752
tomoki_sun <tomoki69386@gmail.com>
2020-01-21 22:21:31 +0900

    Update

--- GitHub/Views/SearchUser/SearchUserViewController.swift
+++ GitHub/Views/SearchUser/SearchUserViewController.swift
@@ -10,9 +10,29 @@ import GitHubCore
 
 final class SearchUserViewController: UIViewController, SearchUserViewProtocol {
     
+    private let tableView = UITableView(frame: .zero, style: .grouped)
+    
     var presenter: SearchUserPresenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.tableFooterView = .init()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension SearchUserViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
